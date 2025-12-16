10f2e50c7e8b1969085d384789c050c49dc1f1a0
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2025-12-16 18:00:06 +0000

    added the types/schemas for it

---
+++ src/trading/types.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+
+export const OrderSideSchema = z.enum(['BUY', 'SELL']);
+export type OrderSide = z.infer<typeof OrderSideSchema>;
+
+export const OrderTypeSchema = z.enum(['LIMIT','MARKET']);
+export type OrderType = z.infer<typeof OrderTypeSchema>;
+
+export const OrderStatusSchema = z.enum(['PENDING', 'OPEN', 'PARTIALLY_FILLED', 'FILLED', 'CANCELLED']);
+export type OrderStatus = z.infer<typeof OrderStatusSchema>;
+
+export const InstrumentSchema = z.enum(['AAPL']);
+export type Instrument = z.infer<typeof InstrumentSchema>;
+
+export const OrderSchema = z.object({
+  id: z.string(),
+  side: OrderSideSchema,
+  type: OrderTypeSchema,
+  status: OrderStatusSchema,
+  instrument: InstrumentSchema,
+  price: z.number().positive().optional(),
+  quantity: z.number().positive(),
+  remainingQuantity: z.number().nonnegative(),
+  timestamp: z.number(),
+});
+
+export type Order = z.infer<typeof OrderSchema>;
+
+export const TradeSchema = z.object({
+    id: z.string(),
+    price: z.number().positive(),
+    quantity : z.number().positive(),
+    instrument : InstrumentSchema,
+    sellOrderId: z.string(),
+    buyOrderId: z.string(),
+    timestamp : z.number(),
+});
+
+export type Trade = z.infer<typeof TradeSchema>;
